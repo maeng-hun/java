59172166d40c5b0d6819d775b18edc9b20b80747
MyungHoon <audgns1640@gmail.com>
2025-11-20 23:33:35 +0900

    2025/11/20 ì다형성 역할,구현

---
+++ src/basic2/poly/car0/K3Car.java
@@ -0,0 +1,15 @@
+package basic2.poly.car0;
+
+public class K3Car {
+
+    public void startEngine() {
+        System.out.println("k3 스타트");
+    }
+    public void offEngine() {
+        System.out.println("k3 스탑");
+    }
+    public void pressAccelerator() {
+        System.out.println("k3 pressAccelerator");
+    }
+
+}
